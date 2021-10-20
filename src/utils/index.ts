6a31ed6c667b3f32e7b7558e0fba724287ef0b78
David Blandón <djesus1906@protonmail.com>
2021-10-20 19:48:54 +0200

    Types definitions and number formatting

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
+const formatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 export function extractText(text: string) {
   const match = text.match(/(?:"[^"]*"|^[^"]*$)/);
   if (match) {
@@ -6,8 +11,28 @@ export function extractText(text: string) {
   return '';
 }
 
-export function parseNumericValue(text: string, lon: number) {
-  return text.split('.').join('').padStart(lon, '0');
+export function parseNumericValue(value: string | number, lon: number) {
+  let toParse: string;
+  if (typeof value === 'number') {
+    toParse = formatter.format(value);
+  } else {
+    toParse = formatter.format(parseFloat(value));
+  }
+  return toParse.split('.').join('').padStart(lon, '0');
+}
+
+export function sumFields(fieldA: Field, fieldB: Field): number {
+  if (typeof fieldA === 'string' && typeof fieldB === 'string') {
+    return parseFloat(fieldA) + parseFloat(fieldB);
+  } else if (typeof fieldA === 'string' && typeof fieldB === 'number') {
+    return parseFloat(fieldA) + parseFloat(formatter.format(fieldB));
+  } else if (typeof fieldA === 'number' && typeof fieldB === 'string') {
+    return parseFloat(formatter.format(fieldA)) + parseFloat(fieldB);
+  } else if (typeof fieldA === 'number' && typeof fieldB === 'number') {
+    return parseFloat(formatter.format(fieldA)) + parseFloat(formatter.format(fieldB));
+  } else {
+    return 0;
+  }
 }
 
 export const blankKeywords = ['BLANCOS', 'blanco', 'En blanco', 'X'];
--- types/common/main.d.ts
+++ types/common/main.d.ts
@@ -1,4 +1,13 @@
 type Period = '1T' | '2T' | '3T' | '4T';
+type DeclarationType = 'I' | 'U' | 'G' | 'N';
+type Field = string | number;
+
+interface Declarant {
+  name: string;
+  lastname: string;
+  nif: string;
+  iban: string;
+}
 
 interface ModelInput {
   exercise: string;
@@ -8,11 +17,15 @@ interface ModelInput {
 }
 
 interface Model111Input extends ModelInput {
-  earnedIncomes: {
-    [key: string]: string;
-  };
-  economicEarnings: {
-    [key: string]: string;
+  declarationType: DeclarationType;
+  declarant: Declarant;
+  fields: {
+    field01: Field;
+    field02: Field;
+    field03: Field;
+    field07: Field;
+    field08: Field;
+    field09: Field;
   };
 }
 
