0ff3d2cd76bd43ce7a4f58b1f1a6c63b8d8529f3
David Blandón <djesus1906@protonmail.com>
2021-10-20 10:26:44 +0200

    Re organized

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const aeatUtils = require("./utils/index");
+const models = require("./models");
 
-const file111 = `${process.cwd()}/models/dr111e16v18.xlsx`;
+const file111 = `${process.cwd()}/specs/dr111e16v18.xlsx`;
 
 async function bootstrap() {
-  await aeatUtils.model111(file111);
+  await models.model111(file111);
 }
 
 bootstrap();
---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  model111: require("./111").default,
+};
