84b33efac35c268d95431233a779e482a304c5ce
David Blandón <djesus1906@protonmail.com>
2021-10-22 20:31:34 +0200

    Added typings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import { model111 } from './models/111';
-import { model115 } from './models/115';
-import { model130 } from './models/130';
-import { model303 } from './models/303';
-export { model111, model115, model130, model303 };
+export * from './models';
+export * from './utils';
---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+import { model111 } from './111';
+import { model115 } from './115';
+import { model130 } from './130';
+import { model303 } from './303';
+export { model111, model115, model130, model303 };
